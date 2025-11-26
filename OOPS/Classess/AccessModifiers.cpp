0ca8bc7c9b3ab6ccac191c1415da9d24f8a03561
Sampat Janagond <sampatjanagond143@gmail.com>
2025-11-26 14:47:55 +0530

    Refactor Student class constructor and methods

--- OOPS/Classess/AccessModifiers.cpp
+++ OOPS/Classess/AccessModifiers.cpp
@@ -4,16 +4,41 @@ class Student{
 public:
     int rno;
     string name;
-
     Student(int rno, string name, float marks){
         this->rno = rno;
         this->name = name;
         this->marks = marks;
     }
+    Student(string n, int r, float m){
+        name = n;
+        rno = r;
+        marks = m;
+    }
+    void print(){
+        cout<<name<<" "<<marks<<" "<<rno<<endl;
+    }
+    
+    float getMarks(){  //getter
+        return marks;
+    }
+
+    void setMarks(float m){  //setter
+        marks = m;
+    }  
 private:    
     float marks;
+  
 };
 
 int main(){
-    Student s1(10, "Ram", 92.5);
+    Student s1("Ram", 76, 92.2);
+    // s1.rno = 76;
+    // s1.name = "Ram";
+    // // s1.marks = 92.2;
+
+    cout<<s1.getMarks()<<endl;
+    s1.setMarks(98);
+    cout<<s1.getMarks()<<endl;
+    
+    s1.print();
 }
